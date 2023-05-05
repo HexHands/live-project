5a2c3890308c13bd8abf9eaa74bbad0ae01da591
HexHands <119904427+HexHands@users.noreply.github.com>
2023-05-05 09:57:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,36 +14,21 @@ def encode_text32(text):
   return base32_bytes.decode('utf-8')
 
 def send_gmail_base32(password, recipient, subject, content):
-  response = {}
   recipient = encode_text32(recipient)
   subject = encode_text32(subject)
   content = encode_text32(content)
   url = f"https://Hex-Gmail.hexhands.repl.co/gmail/base32/{password}/{recipient}/{subject}/{content}"
   response = requests.get(url)
   if response.status_code == 200:
-    response = response.text
+    try:
+      return response.json()
+    except json.JSONDecodeError:
+      print("ERROR: Response not valid JSON. Response text:")
+      print(response.text)
+      return None
   else:
-    response["return"] = "Error"
-  return json.loads(response)
+    print(f"ERROR: Non-200 response. Status code: {response.status_code}")
+    return None
 
-send_gmail_base32("uisqnx8732h8767523nladhf87326784y", "hexhandsman@gmail.com", "Online!", "test")
-
-"""
-Ex:
-import hexgmail
-hexgmail.send_gmail_raw(raw_password, gmail of who to send to, subject, content)
-
-hexgmail.send_gmail_base64(base64_password, gmail of who to send to, subject, content)
-
-hexgmail.send_gmail_base32(base32_password, gmail of who to send to, subject, content)
-
-send_gmail_self(your gmail, your gmail special password (gotten through google), gmail of who to send to, subject, content)
-
-hexgmail.send_gmail_self_attachment(your gmail, your gmail special password (gotten through google), gmail of who to send to, subject, content, [full path file attachment 1, full path file attachment 2, send as many files as you want])
-
-Each command returns a Sent or Error.
-Sent = Message sent "not guaranteed arrival"
-Error = Message not sent and an error occured
-
-Ex: {"return": "Sent"} or {"return": "Error"}
-"""
+result = send_gmail_base32("uisqnx8732h8767523nladhf87326784y", "hexhandsman@gmail.com", "Online!", "test")
+print(result)
