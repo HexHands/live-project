4f88a2bff754f3e530aeef0ea85e75715d71fd89
HexHands <119904427+HexHands@users.noreply.github.com>
2023-05-05 11:20:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ print(project.loves)
 print(project.favorites)
 
 project.set_title(f"❤️{project.loves}  | ⭐{project.favorites} | ◉{project.views}")
+
+project.set_instructions(f"""This project updates every 5-10 minutes!
+
+Currently this project has...
+{project.views} project views!
+{project.loves} loves!
+{project.favorites} favorites!
+
+Thanks to TimMcCool for scratchattach!
+""")
