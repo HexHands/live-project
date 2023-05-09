3574e7b993a562cc35460ece6e61a066c1252778
HexHands <119904427+HexHands@users.noreply.github.com>
2023-05-08 20:28:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import scratchattach as scratch3
 import os
+import thumbnail
 
-session = scratch3.login("ScratchAttachAccount", os.environ['PASSWORD'])
+try:
+  session = scratch3.login("ScratchAttachAccount", os.environ['PASSWORD'])
 
-project = session.connect_project("694515459")
+  project = session.connect_project("694515459")
 
-print(project.views)
-print(project.loves)
-print(project.favorites)
+  print(project.loves)
+  print(project.favorites)
+  print(project.views)
 
-project.set_title(f"❤️ {project.loves}  | ⭐ {project.favorites} | ◉ {project.views}")
+  project.set_title(f"❤️ {project.loves}  | ⭐ {project.favorites} | ◉ {project.views}")
 
-project.set_instructions(f"""This project updates every 5-10 minutes!
+  project.set_instructions(f"""This project updates every 5-15 minutes!
 
-Currently this project has...
-{project.views} project views!
-{project.loves} loves!
-{project.favorites} favorites!
+  Currently this project has...
+  {project.views} project views!
+  {project.loves} loves!
+  {project.favorites} favorites!
 
-Thanks to TimMcCool for scratchattach!
-""")
+  Thanks to TimMcCool for scratchattach!
+  """)
+
+  create_thumbnail(project.loves, project.favorites, project.views)
+
+  project.set_thumbnail(file="thumbnail.png")
+  
+except Exception as e:
+  print(e)
